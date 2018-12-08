04b36818e78be7722ee555e3611d122798b00cca
blueaga003 <blue_aga003@yahoo.com>
2018-12-08 23:08:46 +0000

    Clean up server

--- server.py
+++ server.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 from flask import Flask, redirect, session, request, render_template
 from flask_debugtoolbar import DebugToolbarExtension
-#from jinja2 import StrictUndefined
 
 #TODO: Import secret key and change to variable
 
 app = Flask(__name__)
-#app.jinja_env.undefined = StrictUndefined
-#app.jinja_env.auto_reload = True
+app.secret_key = 'AB'
 
-app.secret_key = "AB"
-
-@app.route('/homepage')
+@app.route('/')
 def view_homepage():
     """Views homepage of app."""
     return render_template('homepage.html')
