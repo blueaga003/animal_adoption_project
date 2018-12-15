6687b7eb9ad0b22b2c06287836c7558a0f04315e
blueaga003 <blue_aga003@yahoo.com>
2018-12-15 18:58:18 +0000

    Add map to frontend

---
+++ frontend/src/components/map/map.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+import { keys } from '../../secret_keys'; 
+// import mapStyle from './mapStyle'; // add a mapStyle
+// const mapStyle = mapStyle.styleArray;
+
+class Map extends Component {
+    static defaultProps = {
+        center: {
+            lat: 37.77495,
+            lng: -122.4194 
+
+        },
+        zoom: 14
+    };
+
+  render() {
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: '1000px', width: '1000px' }}>
+        <h1> MEEEEP </h1>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: keys.google_maps_key }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+          containerElement={ <div style={ { height: "100%" } } /> } 
+        >
+        < /GoogleMapReact>
+      </div>
+    );
+  }
+}
+
+export default Map;
