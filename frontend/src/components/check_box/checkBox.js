1b1fcea909a5efb6b69c72035e53499aa6841a4d
blueaga003 <blue_aga003@yahoo.com>
2018-12-16 00:12:47 +0000

    Make checkbox clickable

--- frontend/src/components/check_box/checkBox.js
+++ frontend/src/components/check_box/checkBox.js
@@ -3,18 +3,18 @@ import React from 'react'
 const CheckBox = (props) => {
 
     return( <div>
-    <label for={props.name} className="form-label">{props.title}</label>
+    <label form={props.name} className="form-label">{props.title}</label>
     <div className="checkbox-group">
       {props.options.map(option => {
         return (
           <label key={option}>
             <input
               className="form-checkbox"
-              id = {props.name}
+              id ={props.name}
               name={props.name}
               onChange={props.handleChange}
               value={option}
-              checked={ props.selectedOptions.indexOf(option) > -1 }
+              checked={props.selectedOptions.indexOf(option) > -1}
               type="checkbox" /> {option}
           </label>
         );
