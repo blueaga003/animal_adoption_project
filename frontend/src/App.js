8244534447a0d783bd82f0266bb159e5e06416a5
blueaga003 <blue_aga003@yahoo.com>
2018-12-21 04:27:51 +0000

    Add routing to routes for views

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,23 @@
 import React, { Component } from 'react';
-import { Container, Row, Col } from 'reactstrap';
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavBar from './components/nav_bar/navBar';
-import Greeting from './components/greeting/greeting';
-import Map from './components/map/map';
-import FormContainer from './containers/searchFormContainer';
+import HomePage from './containers/homepageContainer.js'
 import LogInContainer from './containers/logInContainer';
 // import other components we want to render here such as a NAVBAR for example
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <NavBar/>
-        </header>
-              <div style={{height: '1200px', width: '1200px'}}>
-                <Greeting/>
-                <Container>
-                  <Row>
-                    <Col md="3">
-                      <FormContainer/>
-                    </Col>
-                    <Col md="9">
-                      <Map/>
-                    </Col>
-                  </Row>
-                </Container>
-             </div>
-          <LogInContainer/>
-     </div>
+      <Router>
+        <div className='App'>
+          <header className='App-header'>
+            <NavBar/>
+          </header>
+       <Route exact={true} path='/' component = { HomePage } />
+       <Route path='/login' component = { LogInContainer } />
+       </div>
+     </Router>
     );
   }
 }
---
+++ frontend/src/containers/homepageContainer.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import Greeting from '../components/greeting/greeting'
+import Map from '../components/map/map';
+import FormContainer from './searchFormContainer'
+
+class HomePage extends Component {
+  render() {
+    return (
+        <div style={{height: '1200px', width: '1200px'}}>
+          <Greeting/>
+          <Container>
+            <Row>
+              <Col md="3">
+                <FormContainer/>
+              </Col>
+              <Col md="9">
+                <Map/>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+    );
+  }
+}
+export default HomePage;
