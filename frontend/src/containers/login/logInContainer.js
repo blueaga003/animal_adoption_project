0eb09c0cf2e08cb10fad7a15fabb5be32c15bdd3
blueaga003 <blue_aga003@yahoo.com>
2019-01-04 04:36:14 +0000

    Seperate logic from rendering, update class to be function

---
+++ frontend/src/containers/login/logInContainer.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import Login from "./login";
+//import "./Login.css";
+
+export default class LoginContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: ""
+    };
+
+    this.validateForm = this.validateForm.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  validateForm() {
+    return this.state.email.length > 0 && this.state.password.length > 0;
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+    <Login 
+      email={this.state.email}
+      password={this.state.password}
+      handleChange={this.handleChange}
+      validateForm={this.validateForm}
+    //  handleSubmit={this.handleSubmit}
+    />
+    );
+  }
+}
---
+++ frontend/src/containers/login/login.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+//import "./Login.css";
+
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <div className="Login">
+        <form onSubmit={this.props.handleSubmit}>
+          <FormGroup controlId="email" bsSize="large">
+            <ControlLabel>Email</ControlLabel>
+            <FormControl
+              autoFocus
+              type="email"
+              value={this.props.email}
+              onChange={this.props.handleChange}
+            />
+          </FormGroup>
+          <FormGroup controlId="password" bsSize="large">
+            <ControlLabel>Password</ControlLabel>
+            <FormControl
+              value={this.props.password}
+              onChange={this.props.handleChange}
+              type="password"
+            />
+          </FormGroup>
+          <Button
+            block
+            bsSize="large"
+            disabled={!this.props.validateForm()}
+            type="submit"
+          >
+            Login
+          </Button>
+        </form>
+      </div>
+    );
+  }
+}
