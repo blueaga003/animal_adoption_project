30a8764bb5df40b46d9d1ff6a40eeab4c7e1cecf
blueaga003 <blue_aga003@yahoo.com>
2019-01-06 22:08:16 +0000

    Add login page CSS

--- frontend/src/containers/login/login.js
+++ frontend/src/containers/login/login.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
-//import "./Login.css";i 
+import "./login.css";
 
 export default class Login extends Component {
   constructor(props) {
