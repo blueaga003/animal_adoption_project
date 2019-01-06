87106c779c087ad3305df1b585ec2c70d43b7714
blueaga003 <blue_aga003@yahoo.com>
2019-01-06 01:06:49 +0000

    Add first GET route and delete previous unused routes

--- server/server.py
+++ server/server.py
@@ -1,33 +1,20 @@
 #!/usr/bin/python3
 from flask import Flask, redirect, session, request, render_template
 from flask_debugtoolbar import DebugToolbarExtension
-
+#from model import connect_to_db, User, Location, Rating, PetPreference, Comment, Pet 
 #TODO: Import secret key and change to variable
 
 app = Flask(__name__)
 app.secret_key = 'AB'
 
-@app.route('/')
-def view_homepage():
-    """Views homepage of app."""
-    return render_template('homepage.html')
+@app.route('/signup', methods = ['GET'])
+def login():
+    """User login."""
 
+    print("success")
+    return "hi"
+    
 #Set session/cookie
-
-@app.route('/profile_sign_up')
-def view_profile_sign_up():
-    """Views profile set up page."""
-    return render_template('profile_set_up.html')
-
-@app.route('/search_pets')
-def view_pet_search_page():
-    """Views pet search page."""
-    return render_template('pet_search.html')
-
-@app.route('/search_location')
-def view_location_search_page():
-    """Views location search page."""
-    return render_template('location_search.html')
 
 
 if __name__ == "__main__":
