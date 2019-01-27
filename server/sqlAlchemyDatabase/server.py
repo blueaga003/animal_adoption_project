474dd31cff635c7d52d122ec80a64b264100cc14
blueaga003 <blue_aga003@yahoo.com>
2019-01-27 18:04:27 +0000

    Change "signup" to "register" on backend and clean up register endpoint

--- server/sqlAlchemyDatabase/server.py
+++ server/sqlAlchemyDatabase/server.py
@@ -17,48 +17,29 @@ app.secret_key = 'AB'
 
 
 # Endpoints
-@app.route('/signup', methods = ['GET', 'POST'])
-def signup():
-    """User signup."""
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+    """User register."""
     if request.method == 'POST':
         data = request.get_json()
         user_email = data['email']
-      #  print("USER EMAIL " + str(user_email))
         user_exist_query = db.session.query(User).filter_by(email=user_email).first()
-      #  print("USER EXISTS QUERY: " + str(user_exist_query))
         if user_exist_query == None:
             new_user = []
             new_user = User(email=data['email'], password=data['password'], first_name=data['firstName'], last_name=data['lastName'], default_location='San Francisco')
-         #   print(data['species'])
-         #   print(data['gender'])
-         #   print(data['animalActivityLevels'])
-         #   print(data)
-            #if data['species'] == []:
- #          if data['gender'] == []:
- #              data['gender'] = 'AAA'
- #          if data['animalActivityLevels'] == []:
- #              data['animalActivityLevels'] = 'AAA'
-            #if data['age'] == []:
-            #    data['age'] = 'AAA'
             db.session.add(new_user)
             db.session.commit()
             access_token = create_access_token(identity = data['email'])
             refresh_token = create_refresh_token(identity = data['password'])
-       #     print("GENDER" + str(data['gender']))
             new_user_preferences = PetPreference(user_id=new_user.user_id, species=data['species'],  activity_level=data['animalActivityLevels'], sex=data['gender']) #add age and other columns
        #     print(new_user_preferences)
             db.session.add(new_user_preferences)
             db.session.commit()
             return jsonify({'user':data['email'], 'access_token': access_token, 'refresh_token': refresh_token})
         else:
-       #     print("HIT HERE!!!!")
             user_info={'error': 'User already exists'}
             return jsonify(user_info)
 
-#put in session as logged in
-   # print("success signup")
-   # return "hi"
-    
 @app.route('/login', methods = ['GET', 'POST'])
 #@jwt_required
 def login():
