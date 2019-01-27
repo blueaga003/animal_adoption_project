03d58819bcc888282858d6964897976056dcf520
blueaga003 <blue_aga003@yahoo.com>
2019-01-27 22:17:36 +0000

    Check point in updating NavBar after log in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,12 +9,28 @@ import DisplayResults from './containers/resultsContainer';
 // import other components we want to render here such as a NAVBAR for example
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loggedIn: false
+    }
+  }
+
+checkIfLoggedIn = event => {
+  let accessToken = localStorage.getItem('Token')
+  console.log("AccessToken")
+  console.log(accessToken)
+  if (accessToken !== null && this.state.loggedIn === false) {
+    this.setState({loggedIn: true})
+  }
+}
   render() {
     return (
       <Router>
         <div className='App'>
           <header className='App-header'>
-            <NavBar/>
+            {this.checkIfLoggedIn()}
+            <NavBar loggedIn={this.state.loggedIn}/>
           </header>
        <Route exact={true} path='/' component = { HomePage } />
        <Route path='/petSearch' component = { DisplayResults } />
--- frontend/src/components/nav_bar/navBar.js
+++ frontend/src/components/nav_bar/navBar.js
@@ -1,36 +1,40 @@
 import React, { Component } from 'react';
-import { Collapse, Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 import './navBar.css'
 class NavBar extends Component {
 
-constructor(props) {
-  super(props);
+  renderNavBar = (event) => {
+    console.log("LoggedIn Props")
+    console.log(this.props.loggedIn)
+    if (this.props.loggedIn === true) {
+      return (
+        <Nav className="ml-auto" navbar>
+          <NavItem>
+            <NavLink href="/profile">Profile</NavLink>
+          </NavItem>
+        </Nav>
+      );
+    }
+    else {
+      return (
+        <Nav className="ml-auto" navbar>
+          <NavItem>
+            <NavLink href="/register">Register</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/login">Login</NavLink>
+          </NavItem>
+        </Nav>
+      );
+    }
+  }
 
-  this.toggle = this.toggle.bind(this);
-  this.state = {
-     isOpen: false
-   };
- }
-toggle() {
-  this.setState({
-    isOpen: !this.state.isOpen
-  });
-}
 render() {
   return (
     <div>
       <Navbar color="faded" light expand="md">
         <NavbarBrand href="/">HoundDog</NavbarBrand>
-        <Collapse isOpen={this.state.isOpen} navbar>
-          <Nav className="ml-auto" navbar>
-            <NavItem>
-              <NavLink href="/register">Register</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/login">Login</NavLink>
-            </NavItem>
-          </Nav>
-        </Collapse>
+        {this.renderNavBar()}
       </Navbar>
     </div>
   );
