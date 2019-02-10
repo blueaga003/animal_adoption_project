30db904a9aaf6ab60fd3e153818fd533d935082d
blueaga003 <blue_aga003@yahoo.com>
2019-02-10 02:44:08 +0000

    Add Greeting component, delete animalPreferencesContainer2 which was accidently committed

--- frontend/src/components/greeting/greeting.js
+++ frontend/src/components/greeting/greeting.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "./greeting.css";
 
 class Greeting extends Component {
   render() {
     return (
-      <div className = "Greeting">
-        <br/>
+      <div className = "greeting">
+        <h3> Welcome to HoundDog </h3>
+        We are your one stop shop for finding shelter pets!<br/>
+        Simply create an account and let the searching begin!
         <br/>
       </div> 
     );
